17a6b4a3053e9e9fe1458b6385f0db1591b8304c
kv-18-vk <cse240001040@iiti.ac.in>
2025-11-03 15:06:25 +0530

    osskk

--- server.js
+++ server.js
@@ -464,8 +464,10 @@ app.post('/report/monthly-stats', express.json(),  (req, res) => {
       COALESCE(SUM(sales_profit), 0) AS total_profit,
       COALESCE(SUM(expired_loss), 0) AS total_loss
     FROM (
-      SELECT MONTH(date) as month, SUM(revenue) AS sales_profit, 0 AS expired_loss
-      FROM sales WHERE YEAR(date) = ?
+      SELECT MONTH(date) as month, (SUM(s.revenue)-SUM(s.quantity_sold * d.cp)) AS sales_profit, 0 AS expired_loss
+      FROM sales s
+      JOIN delivery d ON s.product_id = d.product_id
+      WHERE YEAR(date) = ?
       GROUP BY month
       UNION ALL
       SELECT MONTH(date_expired) as month, 0 AS sales_profit, SUM(loss) AS expired_loss
@@ -488,8 +490,9 @@ app.get('/report/yearly-stats', express.json() , (req, res) => {
       COALESCE(SUM(sales_profit), 0) AS total_profit,
       COALESCE(SUM(expired_loss), 0) AS total_loss
     FROM (
-      SELECT YEAR(date) as year, SUM(revenue) AS sales_profit, 0 AS expired_loss
-      FROM sales
+      SELECT YEAR(s.date) as year, (SUM(s.revenue)-SUM(s.quantity_sold * d.cp)) AS sales_profit, 0 AS expired_loss
+      FROM sales s
+      JOIN delivery d ON s.product_id = d.product_id
       GROUP BY year
       UNION ALL
       SELECT YEAR(date_expired) as year, 0 AS sales_profit, SUM(loss) AS expired_loss
